a7eac2870e6977f9c56ff2949dd9efdab8de1bcc
Manish Ojha <120119872+mnishojha@users.noreply.github.com>
2025-07-29 01:29:50 +0530

    Initial Commit

---
+++ Pantry/PantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryApp.swift
+//  Pantry
+//
+//  Created by manish ojha on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
